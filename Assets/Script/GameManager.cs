f0ea6689240dc6889a1cc16dd1576fd40dcfb13b
SiwakornKuapunya <siwakorn.kuap@bumail.net>
2023-11-27 23:36:14 +0700

    Finish

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,25 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    private int playScore;
-    public int PlayScore 
-    { 
-        get { return playScore; } 
-        set { playScore = value; }
-    }
+    private int playerScore;
+    public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
+
     [SerializeField]
     private GameObject ballPrefab;
+
     [SerializeField]
     private GameObject[] ballPositions;
-    public static GameManager Instance;
 
+    [SerializeField]
+    private GameObject cueBall;
+
+    [SerializeField]
+    private GameObject ballLine;
+
+    [SerializeField]
+    private float xInput;
+
+    [SerializeField]
+    private GameObject camera;
+
+    [SerializeField]
+    private TMP_Text scoreText;
+
+    public static GameManager instance;
     void Start()
     {
-        Instance = this;
-        SetBall(BallColor.White, 0);
+        instance = this;
+
+        UpdateScoreText();
+
+        camera = Camera.main.gameObject;
+        CameraBehindCueBall();
         SetBall(BallColor.Red, 1);
         SetBall(BallColor.Yellow, 2);
         SetBall(BallColor.Green, 3);
@@ -29,16 +48,63 @@ public class GameManager : MonoBehaviour
         SetBall(BallColor.Black, 7);
     }
 
-void Update()
-{
-
-}
-    private void SetBall(BallColor col, int i)
+    void Update()
     {
-        GameObject obj = Instantiate(ballPrefab,
-            ballPositions[i].transform.position,
-            Quaternion.identity);
+        RotateBall();
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+            ShootBall();
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+            StopBall();
+    }
+
+    private void SetBall(BallColor col,int i)
+    {
+       GameObject obj = Instantiate(ballPrefab,
+                                    ballPositions[i].transform.position,
+                                    Quaternion.identity);
         Ball b = obj.GetComponent<Ball>();
         b.SetColorAndPoint(col);
+    }
+
+    private void RotateBall()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        cueBall.transform.Rotate(new Vector3(0f,xInput/20f , 0f));
+    }
+
+    private void ShootBall()
+    {
+        camera.transform.parent = null;
+        Rigidbody rb = cueBall.GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
+
+        ballLine.SetActive(false);
+    }
+
+    private void CameraBehindCueBall()
+    {
+        camera.transform.parent = cueBall.transform;
+        camera.transform.position = cueBall.transform.position
+                                    + new Vector3(0f, 7f, -10f);
+    }
+
+    private void StopBall()
+    {
+        Rigidbody rb = cueBall.GetComponent<Rigidbody> ();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        cueBall.transform.eulerAngles = Vector3.zero;
+
+        CameraBehindCueBall();
+        camera.transform.eulerAngles = new Vector3(30f, 0f, 0f);
+
+        ballLine.SetActive (true);
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = $"Player Score:{playerScore}";
     }
 }
