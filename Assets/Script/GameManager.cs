b83ea7b66ccffe3b577f4bd55db82b557085db1c
SiwakornKuapunya <siwakorn.kuap@bumail.net>
2023-11-27 22:35:57 +0700

    Ball done commit

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private int playScore;
+    public int PlayScore 
+    { 
+        get { return playScore; } 
+        set { playScore = value; }
+    }
+    [SerializeField]
+    private GameObject ballPrefab;
+    [SerializeField]
+    private GameObject[] ballPositions;
+    public static GameManager Instance;
+
     void Start()
     {
-        
+        Instance = this;
+        SetBall(BallColor.White, 0);
+        SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
     }
 
-    // Update is called once per frame
-    void Update()
+void Update()
+{
+
+}
+    private void SetBall(BallColor col, int i)
     {
-        
+        GameObject obj = Instantiate(ballPrefab,
+            ballPositions[i].transform.position,
+            Quaternion.identity);
+        Ball b = obj.GetComponent<Ball>();
+        b.SetColorAndPoint(col);
     }
 }
